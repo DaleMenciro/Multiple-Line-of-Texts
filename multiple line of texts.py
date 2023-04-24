7b0f2cd0c618f6728a9901168770c4f5342c68ae
Dale Menciro <dale_menciro@protonmail.com>
2023-04-24 22:16:08 +0800

    Pseudocode

--- multiple line of texts.py
+++ multiple line of texts.py
@@ -8,3 +8,14 @@ Are there more lines y/n? y
 Enter line: Simple is better than complex.
 Are there more lines y/n? n 
 '''
+
+#open file for writing
+    #loop until the user is done entering
+        #asks user to input the line
+        #write the line to the file
+
+        #loop until the user input valid response y/n
+            #asks user if they want to input more lines
+            #if input is valid, loop breaks
+            #if not, prompt the user again
+        #if user says no, break out the loop

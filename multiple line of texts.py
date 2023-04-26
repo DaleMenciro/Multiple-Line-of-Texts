1c1bf6540a4cf3ba60deef25ad1c7e3568ff8d48
Dale Menciro <dale_menciro@protonmail.com>
2023-04-26 20:59:37 +0800

    fixing colorama functions

--- multiple line of texts.py
+++ multiple line of texts.py
@@ -7,22 +7,23 @@ def write_file():
         #loop until the user is done entering
         while True:
             #asks user to input the line
-            line_input = input("Enter line: ", "blue")
+            line_input = input(colored("Enter line: ", "blue"))
             #write the line to the file
             my_file.write(line_input + "\n")
             #loop until the user input valid response y/n
             while True:
                 #asks user if they want to input more lines
-                response = input("Are there more lines (y/n)? ", "yellow")
+                response = input(colored("Are there more lines (y/n)? ", "yellow"))
                 response = response.lower
                 #if input is valid, loop breaks
                 if response() == "y" or response() == "n":
                     break
                 #if not, prompt the user again
-                print("Invalid response! Please input 'y' or 'n' only: ", "red")
+                print(colored("Invalid response! Please input 'y' or 'n' only: ", "red"))
             #if user says no, break out the loop
             if response() == "n":
                 break
 
 #==== START ====
+colorama.init()
 write_file()
